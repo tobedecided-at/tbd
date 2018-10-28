9ba04f89a8c1c0e7e45f73b197179579edd64a50
DatNoHand <gabriel@selinschek.com>
2018-10-29 00:06:21 +0100

    merge master to terrain_gen

--- Assets/Scripts/Classes/TBDHelper.cs
+++ Assets/Scripts/Classes/TBDHelper.cs
@@ -10,3 +10,10 @@ public class TBDHelper : MonoBehaviour {
     TBDBootstrap.NewGame();
   }
 }
+
+public static class TBDExtensions {
+
+  public static float Map(this float f, float from1, float to1, float from2, float to2) {
+    return (f - from1) / (to1 - from1) * (to2 - from2) + from2;
+  } 
+}
