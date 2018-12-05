81f053006daa39de8e92c307a99d21c38a93b1c0
DatNoHand <gabriel@selinschek.com>
2018-12-05 15:47:23 +0100

    [MERGE] compass : master

--- Assets/Scripts/Systems/PlayerHUDSystem.cs
+++ Assets/Scripts/Systems/PlayerHUDSystem.cs
@@ -20,6 +20,7 @@ public class PlayerHUDSystem : ComponentSystem {
       var cHealth = trPlayer.GetComponent<Health>();
       var cArmor = trPlayer.GetComponent<Armor>();
       var rImgCompass = inventoryUI.rImgCompass;
+      var tCompassDir = inventoryUI.tCompassDir;
 
       inventoryUI.sHealth.maxValue = cHealth.max;
       inventoryUI.sHealth.value = cHealth.value;
@@ -30,6 +31,48 @@ public class PlayerHUDSystem : ComponentSystem {
       inventoryUI.tArmor.text = cArmor.value.ToString();
 
       rImgCompass.uvRect = new Rect(trPlayer.localEulerAngles.y / 360f, 0, 1, 1);
+
+      Vector3 forward = trPlayer.forward;
+      forward.y = 0;
+
+      float headingAngle = Quaternion.LookRotation(forward).eulerAngles.y;
+      headingAngle = 5 * (Mathf.RoundToInt(headingAngle / 5.0f));
+
+      int displayangele;
+      displayangele = Mathf.RoundToInt (headingAngle);
+
+      switch (displayangele) {
+        case 0:
+          tCompassDir.text = "N";
+          break;
+        case 360:
+          tCompassDir.text = "N";
+          break;
+        case 45:
+          tCompassDir.text = "NE";
+          break;
+        case 90:
+          tCompassDir.text = "E";
+          break;
+        case 130:
+          tCompassDir.text = "SE";
+          break;
+        case 180:
+          tCompassDir.text = "S";
+          break;
+        case 225:
+          tCompassDir.text = "SW";
+          break;
+        case 270:
+          tCompassDir.text = "W";
+          break;
+        case 315:
+          tCompassDir.text = "NW";
+          break;
+        default:
+          tCompassDir.text = headingAngle.ToString ();
+          break;
+      }
     }
   }
 }
