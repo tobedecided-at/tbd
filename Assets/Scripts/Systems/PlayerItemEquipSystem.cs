8fade43365db246a7ca735737c29fa62bf30ffbc
datnohand <gabriel@selinschek.com>
2018-12-10 22:17:47 +0100

    Debug.Log(selectedItem.slug)

---
+++ Assets/Scripts/Systems/PlayerItemEquipSystem.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+using TBD.Items;
+
+public class PlayerItemEquipSystem : ComponentSystem {
+  public struct Data {
+    public readonly int Length;
+    public ComponentArray<PlayerInventory> pi;
+  }
+
+  [Inject] private Data data;
+
+  protected override void OnUpdate() {
+    for (int i = 0; i != data.Length; i++) {
+      var inventory = data.pi[i];
+
+      // If the hotbar item != null
+      Item selected = inventory.inventory[inventory.hotbar.selected];      
+      if (selected != null) {
+        Debug.Log("Item " + selected.slug);
+      }
+    } 
+  }
+}
