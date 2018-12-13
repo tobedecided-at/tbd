05eba0d82631e397d7c69031ba9a2f1fc62e5312
David King <39958050+dkueng01@users.noreply.github.com>
2018-12-13 17:27:21 +0100

    better resolution for healthbar

--- Assets/Scripts/MonoBehavior/Minimap.cs
+++ Assets/Scripts/MonoBehavior/Minimap.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class minimap : MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
    
+    void Start ()
+    {
+        player = TBDBootstrap.Settings.UI.GetComponentInParent<Player>().gameObject;
+    }
+
     void LateUpdate ()
     {
-        Vector3 newPosition = player.position;
+        Vector3 newPosition = player.transform.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
     }
